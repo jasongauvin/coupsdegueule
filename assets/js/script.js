567a6d1dd21dda80b543b7064d27412f3a5658e2
Jason Gauvin <jasongauvin@MacBook-Pro-de-Jason.local>
2019-05-29 23:30:01 +0200

    home + login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+
+/*
 document.querySelector(".button--nav").addEventListener("click", function(){
     document.querySelector(".sidebar").classList.toggle("active");
 });
-
+ */
 document.querySelector(".button--newPost").addEventListener("click", function(){
-    document.querySelector(".post--new").classList.toggle("active");
-    document.querySelector(".header").classList.toggle("header__newpost--open");
+    console.log('coucouc');
+     document.querySelector(".post--new").classList.add("active");
+    document.querySelector(".header").classList.add("header__newpost--open");
 });
-
+/*
 document.querySelector(".button--sendPost").addEventListener("click", function(){
     document.querySelector(".post--new").classList.toggle("active");
     document.querySelector(".header").classList.toggle("header__newpost--open");
@@ -15,3 +18,4 @@ document.querySelector(".button--sendPost").addEventListener("click", function()
 document.querySelector(".botton--close").addEventListener("click", function(){
     document.querySelector(".popup--info").style.right = "-500px";
 });
+ */
